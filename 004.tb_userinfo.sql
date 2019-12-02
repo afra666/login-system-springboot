1a88d10d1fa57b9c9fd7ffc6c626847a80880066
afra666 <48674984+afra666@users.noreply.github.com>
2019-12-01 18:47:04 -0800

    Update 004.tb_userinfo.sql

--- 004.tb_userinfo.sql
+++ 004.tb_userinfo.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS `tb_userinfo`;
-CREATE TABLE `user` (
+CREATE TABLE `tb_userinfo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `addTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
